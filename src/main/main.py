2ccb8dede7514e50dee3038f30f4c973e00ca400
luis00rod <66512866+luis00rod@users.noreply.github.com>
2023-07-29 23:41:15 +0200

    commit notebooks

--- src/main/main.py
+++ src/main/main.py
@@ -3,7 +3,7 @@ from repositories.processed_repository import ProcessedRepo
 from interactors.classificator.data_preparation_titanic import DataPreparationTitanic
 from interactors.forecast.data_preparation_air_quality import DataPreparationAirQuality
 from interactors.classificator.classificator_titanic import ClassificatorTitanic
-from interactors.forecast.forecast_model_neural_network import ModelNN
+from interactors.forecast.forecast_nn import AirQualityPredictor
 from pipelines.forecast_pipeline import Pipeline
 from utils.functions import Functions
 
@@ -19,5 +19,5 @@ class Main(object):
             data_preparation_titanic=DataPreparationTitanic(conf, utils),
             data_preparation_air_quality=DataPreparationAirQuality(conf, utils, raw_repo, processed_repo),
             model_titanic=ClassificatorTitanic(conf),
-            model_air_quality=ModelNN(utils)
+            model_air_quality=AirQualityPredictor()
         )
--- src/main/pipelines/forecast_pipeline.py
+++ src/main/pipelines/forecast_pipeline.py
@@ -14,6 +14,6 @@ class Pipeline(object):
         self.data_preparation_air_quality.apply()
         self.data_preparation_titanic.apply()
         results_titanic = self.model_titanic.apply(data=self.data_preparation_titanic)
-        results_air_quality = self.model_air_quality.apply(data=self.data_preparation_air_quality)
+        results_air_quality = self.AirQualityPredictor.apply(data=self.data_preparation_air_quality)
         return
 
